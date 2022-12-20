84382e8cad5fce84a1de0824c4e0b9fc0ca3be53
okkun <eya0428tky@gmail.com>
2022-12-20 19:45:42 +0900

    connection-fixed

--- index.js
+++ index.js
@@ -4,23 +4,24 @@ const app = express()
 
 app.use(express.static('public'));
 
-const connection =
-   mysql.createConnection({
-      host:'us-cdbr-east-06.cleardb.net',
-      user:'bf14d42cd29744',
-      password:'f0afa56f',
-      database:'heroku_6f1cd1ab31fd623'
+const db_config = {
+   host: 'us-cdbr-east-06.cleardb.net',
+   user: 'bf14d42cd29744',
+   password: 'f0afa56f',
+   database: 'heroku_6f1cd1ab31fd623'
+};
+
+const pool = mysql.createPool(db_config);
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(request, response) {
+   pool.getConnection(function(err, connection){
+       connection.query('SELECT * FROM all_subjects', function(err,results){
+       res.render('top.ejs',{subjects: results});
+       connection.release();
+       });
    });
-
-var port = process.env.PORT || 5000;
-
-app.get('/', (req, res) => {
-    connection.query(
-      'SELECT * FROM all_subjects',
-      (error, results) => {
-        res.render('top.ejs',{subjects: results});
-      }
-    );
- });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
